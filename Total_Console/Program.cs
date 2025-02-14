27dd3bfa868b7c28c7b33a085458bd453e87bb1b
Htoon Lynn Kyaw <htoonlynnkyaw1610@gmail.com>
2025-02-14 15:39:50 +0630

    TestAreaForConsole_140225

---
+++ Total_Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string person = "John Lennon";
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Hello, World! The counter is: " + i);
+}
+Console.WriteLine("Finished printing Hello, World! " + person);
+Console.ReadLine();
